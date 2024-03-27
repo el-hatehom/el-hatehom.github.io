80ad93884aa49416f3ce79d2747c09173b80c527
AdamHoles <adamholes@gmail.com>
2024-03-27 15:31:36 +0100

    I added Students' Table.

---
+++ ea/p17/data.js
@@ -0,0 +1,43 @@
+const wpResults = [
+	{
+		name: "Henry Edwards",
+		grade: 3
+	},
+	{
+		name: "John Young",
+		grade: 2
+	},
+	{
+		name: "Noah Williams",
+		grade: 4
+	},
+	{
+		name: "Julie Moore",
+		grade: 5
+	},
+	{
+		name: "Jeffrey Roberts",
+		grade: 5
+	},
+	{
+		name: "Brandon Turner",
+		grade: 3
+	},
+	{
+		name: "Mia Wright",
+		grade: 5
+	},
+	{
+		name: "Catherine Mitchell",
+		grade: 4
+	},
+	{
+		name: "Kevin Johnson",
+		grade: 4
+	},
+	{
+		name: "Thomas James",
+		grade: 5
+	}
+];
+console.table(wpResults);
